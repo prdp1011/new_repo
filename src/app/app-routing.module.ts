051c9995ee8ea346dd24da0b760610d5622f5425
Pradeep kushwah <pradeepkuswah.it1011@gmail.com>
2018-08-15 16:34:38 +0530

    added repo angular 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first', loadChildren: '../app/first/first.module#FirstModule'},
+  {path: 'second', loadChildren: '../app/second/second.module#SecondModule'},
+  {path: 'third', loadChildren: '../app/third/third.module#ThirdModule'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(private router: Router){
+    let x = ['first', 'second', 'third']
+    let i = 0 ;
+    setInterval(() => {
+    this.router.navigateByUrl(x[i]);
+    i++;
+    }, 10000);
+  }
 }
